c80b1ff7ac49e0b4c7ea6f69e27e22920a81a5f8
mcoral <mcoral>
2024-06-06 15:29:13 -0500

    Séptima prueba

---
+++ src/main/java/com/livevox/integration/shareddata/multiplier/jobs/ClientId.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020. Luis Felipe Sosa Alvarez. All rights reserved to LiveVox.
+ * Use this subject to license terms.
+ *
+ * IS-CALL-MULTIPLIER
+ */
+
+package com.livevox.integration.shareddata.multiplier.jobs;
+
+public interface ClientId  {
+
+
+    public Integer getClientId();
+
+
+    public void setClientId(Integer clientId) ;
+
+}
