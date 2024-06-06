da74722700d01c2e7e13fc5539b245229dfe5bdf
mcoral <mcoral>
2024-06-05 20:19:41 -0500

    Sexta prueba

--- src/main/java/com/livevox/is/metricsservice/domain/AgentRequest.java
+++ src/main/java/com/livevox/is/metricsservice/domain/AgentRequest.java
@@ -3,22 +3,29 @@ package com.livevox.is.metricsservice.domain;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.livevox.integration.commons.domain.lvapi.ListRequest;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
-@ToString( callSuper = true)
-public class AgentRequest  extends ListRequest implements Serializable, Cloneable {
+@EqualsAndHashCode(callSuper=false)
+public class AgentRequest extends ListRequest implements Serializable {
+
+    private RestFilter filter;
     private String application;
     private Date startDate;
     private Date endDate;
     private String account;
     private String originalAccountNumber;
-    private RestFilter filter;
     private String loginId;
     private String lastName;
     private String phoneNumber;
+
 }
